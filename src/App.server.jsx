fdb78f164fd644911564d2b7e97d100a64536ebe
Jomar <jomarhan.ng@gmail.com>
2023-09-25 16:14:23 -0400

    add Not Found Page

--- src/App.server.jsx
+++ src/App.server.jsx
@@ -5,8 +5,10 @@ import {
   FileRoutes,
   ShopifyProvider,
   CartProvider,
+  Route,
 } from "@shopify/hydrogen";
 import { Suspense } from "react";
+import Layout from "./components/Layout.server";
 
 function App() {
   return (
@@ -15,6 +17,7 @@ function App() {
         <CartProvider>
           <Router>
             <FileRoutes />
+            <Route path="*" page={<NotFound />} />
           </Router>
         </CartProvider>
       </ShopifyProvider>
@@ -23,3 +26,13 @@ function App() {
 }
 
 export default renderHydrogen(App);
+
+function NotFound() {
+  return (
+    <Layout>
+      <div className="container">
+        <div>Page not found</div>
+      </div>
+    </Layout>
+  );
+}
